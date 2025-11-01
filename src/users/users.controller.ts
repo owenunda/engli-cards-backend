54f1b938caf906eb2f6c235023cf34650ef50299
owenunda <oweenunda.dev@gmail.com>
2025-11-01 17:21:11 -0500

    doc: docmentado los endpoinst de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,38 +2,51 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto, UpdateUserDto } from './interfaces/user.interface';
 import { UserEntity } from './entities/user.entity';
-import { ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post()
+  @ApiResponse({ status: 201, type: UserEntity })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiOperation({ summary: 'Crea un nuevo usuario' })
+  createUser(@Body() user: CreateUserDto) {
+    return this.usersService.createUser(user);
+  }
 
   @Get()
-  @ApiCreatedResponse({ type: [UserEntity] })
+  // documenting response type for Swagger
+  @ApiOperation({ summary: 'Retorna todos los usuarios' })
+  @ApiResponse({status: 200, type: [UserEntity] })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   getAllUsers() {
     return this.usersService.getAllUsers();
   }
 
   @Get('/:id')
-  @ApiCreatedResponse({ type: UserEntity })
+  @ApiOperation({ summary: 'Retorna un usuario por ID' })
+  @ApiResponse({ status: 200, type: UserEntity })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   getUserById(@Param('id') id: string) {
     return this.usersService.getUserById(Number(id));
   }
 
-  @Post()
-  @ApiCreatedResponse({ type: UserEntity })
-  createUser(@Body() user: CreateUserDto) {
-    return this.usersService.createUser(user);
-  }
+
 
   @Patch('/:id')
+  @ApiOperation({ summary: 'Actualiza un usuario por ID' })
+  @ApiResponse({ status: 200, type: UserEntity })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   updateUser(@Param('id') id: string, @Body() userData: UpdateUserDto) {
     return this.usersService.updateUser(Number(id), userData);
   }
 
   @Delete('/:id')
-  @ApiCreatedResponse({ description: "User deleted successfully" })
+  @ApiOperation({ summary: 'Elimina un usuario por ID' })
+  @ApiResponse({ status: 200, description: "User deleted successfully" })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(Number(id));
   }
